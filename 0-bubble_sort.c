95a49d8e17882df0b51f5e5c79bfcf2c99f6eba2
devmutinda <davidkymutinda@gmail.com>
2022-07-19 16:36:42 +0000

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array using bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, k, hld = size;
+	int temp;
+
+	for (i = 0; i < hld; i++)
+	{
+		for (k = 0; k < hld; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+				print_array(array, size);
+				hld--;
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sorting_algorithms
+This repo contains sorting algorithms written in C language
